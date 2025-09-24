c57644907c956a64df16f7892c1abeef31d58b29
Your Name <you@example.com>
2025-09-24 10:08:08 +0200

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
